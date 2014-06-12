6d970fea60fa15766573a4f56dcfc0ff6cc56e7a
raphaelschmid <raphael_schmid@gmx.net>
2014-06-12 10:58:49 +0200

    asf

--- Meta-Forma/Meta-Forma/Spielfeld.Designer.cs
+++ Meta-Forma/Meta-Forma/Spielfeld.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Spielfeld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Name = "Spielfeld";
             this.Text = "Spielfeld";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
