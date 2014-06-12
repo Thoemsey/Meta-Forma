e513a705b06193e71af2713116fb6ee4157c8450
raphaelschmid <raphael_schmid@gmx.net>
2014-06-12 11:07:12 +0200

    teaedsfa

--- Meta-Forma/Meta-Forma/Schwierigkeitsauswahl.Designer.cs
+++ Meta-Forma/Meta-Forma/Schwierigkeitsauswahl.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 315);
+            this.ClientSize = new System.Drawing.Size(338, 315);
             this.Name = "Schwierigkeitsauswahl";
             this.Text = "Meta-Forma";
             this.ResumeLayout(false);
