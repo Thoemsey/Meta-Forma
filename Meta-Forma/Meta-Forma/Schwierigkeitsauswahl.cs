6390a06099f1d1f6ee26d5cec972d5ad480e0864
raphaelschmid <raphael_schmid@gmx.net>
2014-06-12 11:04:45 +0200

    asdf

---
+++ Meta-Forma/Meta-Forma/Schwierigkeitsauswahl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meta_Forma
+{
+    public partial class Schwierigkeitsauswahl : Form
+    {
+        public Schwierigkeitsauswahl()
+        {
+            InitializeComponent();
+        }
+    }
+}
